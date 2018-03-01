3a71dcf30ae877533cf9f58162c31a4edb5d17b9
bdwest17 <32143067+bdwest17@users.noreply.github.com>
2018-03-01 14:00:52 -0500

    Version 1.0.1
    Added varnish-http-purge functionality to clear varnish cache when pages or posts are updated

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # lnb-varnish-purger
 
 ** Initial Upload.  Only purges the Varnish Cache when all Rocket Cache is cleared. **
+
+## Changelog ##
+
+** Version 1.0.1 <br />
+Added varnish-http-purge to clear cache when posts or pages are updated.  
--- lnb_varnish_purger.php
+++ lnb_varnish_purger.php
@@ -4,22 +4,36 @@
 Plugin Name: LNB Varnish Purger
 Plugin URI: http://www.leadsnearby.com
 Description: Clears the Varnish Cache when the Rocket Cache is Cleared
-Version: 1.0.0
+Version: 1.0.1
 Author: Brian West
 Author URI: http://www.leadsnearby.com
 License: GPLv3
 */
 
-add_action( 'after_rocket_clean_domain', 'purge_varnish_cache');
-
-function purge_varnish_cache() {
-	if (current_user_can('administrator')) {
-		$url = site_url() . "/.*";
-		$response = wp_remote_request( $url,
+class lnbCachePurging {
+    
+    public function __construct () {
+        require_once( plugin_dir_path( __FILE__ ) .'/inc/varnish-http-purge.php' );
+        require_once( plugin_dir_path( __FILE__ ) .'/updater/github-updater.php' );
+        new GitHubPluginUpdater( __FILE__, 'LeadsNearby', 'lnb-varnish-purger' );
+        
+        add_action( 'after_rocket_clean_domain', array($this, 'purge_varnish_cache'));
+        add_action( 'before_run_rocket_sitemap_preload', array($this, 'purge_varnish_cache'));
+        add_action( 'wp_rocket_first_install', array($this, 'purge_varnish_cache'));
+    }
+    
+    public function purge_varnish_cache() {
+        if (current_user_can('administrator')) {
+		    $url = site_url() . "/.*";
+	    	$response = wp_remote_request( $url,
     		array(
-        		'method'   => 'PURGE',
-				'blocking' => false
+                'method'   => 'PURGE',
+			    'blocking' => false
     		)
-		);
-	}
+		    );
+	    }
+    }
 }
+
+new lnbCachePurging();
+
